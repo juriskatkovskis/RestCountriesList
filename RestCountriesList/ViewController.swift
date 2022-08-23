0ddf040ca32b317314f68ae91a2974215abb88a7
juriskatkovskis <juris.katkovskis@accenture.com>
2022-08-23 19:14:12 +0300

    Initial Commit

---
+++ RestCountriesList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestCountriesList
+//
+//  Created by juris.katkovskis on 23/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
